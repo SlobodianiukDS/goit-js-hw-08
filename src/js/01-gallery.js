3b2c173559d4ea97b54e80c682bcb405f6c3db6d
SlobodianiukDS <107937529+SlobodianiukDS@users.noreply.github.com>
2022-11-26 17:21:33 +0200

    tasks 1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryPlace = document.querySelector(`.gallery`);
+
+// console.log(galleryItems);
+
+const changeGalleryOfImage = galleryItems.map(galleryItem => {
+  const changeItemEl = document.createElement(`a`);
+  changeItemEl.href = galleryItem.original;
+  changeItemEl.classList.add(`gallery__link`);
+
+  const changeImgEl = document.createElement(`img`);
+  changeImgEl.classList.add(`gallery__image`);
+  changeImgEl.src = galleryItem.preview;
+  changeImgEl.alt = galleryItem.description;
+  changeImgEl.dataset.source = galleryItem.original;
+
+  changeItemEl.appendChild(changeImgEl);
+
+  return changeItemEl;
+});
+galleryPlace.append(...changeGalleryOfImage);
+
+// ----------------------------------------------------------
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
+
+galleryPlace.addEventListener(`click`, callModalWindow);
+
+function callModalWindow(event) {
+  event.preventDefault();
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import vimeoPlayer from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+onVideoInSaveTime();
+
+player.on('timeupdate', throttle(saveCurentTimeVideo, 1000));
+
+function saveCurentTimeVideo(event) {
+  console.log(event.seconds);
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+
+function onVideoInSaveTime() {
+  const time = localStorage.getItem('videoplayer-current-time');
+  if (time) {
+    player.setCurrentTime(time);
+  }
+}
